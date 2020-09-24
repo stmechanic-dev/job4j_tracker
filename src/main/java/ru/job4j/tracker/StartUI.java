bf9af2ee34d9ed473af0e4eb9a85836a45ecebd6
stmechanic-dev <kovalev.ilya@mail.ru>
2020-09-24 03:05:01 +0300

    6. Date. Отображение даты. [#358466]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item curDate = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = curDate.getCreated().format(formatter);
+        System.out.println(currentDateTimeFormat);
+    }
+}
