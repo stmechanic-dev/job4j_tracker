6d92586c576f5afd15a8ed7f8044fc474c7e1a0d
stmechanic-dev <kovalev.ilya@mail.ru>
2020-09-24 03:04:05 +0300

    1.2. Вызов метода объекта [#358458]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.*;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public int getId() {
         return id;
@@ -19,4 +22,6 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public LocalDateTime getCreated() { return created; }
 }
