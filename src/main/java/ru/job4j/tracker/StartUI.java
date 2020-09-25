1012c0ad1ce0478d2ddb85add76423f9ef75ab96
stmechanic-dev <kovalev.ilya@mail.ru>
2020-09-26 00:30:05 +0300

    6. Метод замены заявки. Tracker.replace [#358481]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,13 +4,13 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item(1, "Ilya");
+//        Item item = new Item(1, "Ilya");
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 //        String currentDateTimeFormat = curDate.getCreated().format(formatter);
 //        System.out.println(currentDateTimeFormat);
 //        System.out.println(curDate);
-        Tracker tracker = new Tracker();
-        tracker.add(item);
-        System.out.println(tracker.findById(1));
+//        Tracker tracker = new Tracker();
+//        tracker.add(item);
+//        System.out.println(tracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,16 +13,20 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll(){
@@ -49,5 +53,15 @@ public class Tracker {
         }
         list = Arrays.copyOf(list, size);
         return list;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        int itemsId = indexOf(id);
+        if (itemsId != -1) {
+            items[itemsId].setName(item.getName());
+            rsl = true;
+        }
+        return rsl;
     }
 }
