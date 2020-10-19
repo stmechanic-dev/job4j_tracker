dd612658ee2c81c6ef88f8a3e61acc10fc465e1c
stmechanic-dev <kovalev.ilya@mail.ru>
2020-10-20 02:21:19 +0300

    3. Банковские переводы [#358394]

---
+++ src/main/java/ru/job4j/tracker/collection/map/BankService.java
@@ -0,0 +1,62 @@
+package ru.job4j.tracker.collection.map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        List<Account> listReq = new ArrayList<>();
+        users.putIfAbsent(user, listReq);
+    }
+
+    public void addAccount(String passport, Account account) {
+        User findUser = findByPassport(passport);
+        List<Account> findListReq = users.get(findUser);
+        if (!findListReq.contains(account)) {
+            users.get(findUser).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        User find = null;
+        for (User u : users.keySet()) {
+            if (u.getPassport().equals(passport)) {
+                find = u;
+                break;
+            }
+        }
+        return find;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account find = null;
+        User findU = findByPassport(passport);
+        List<Account> listReq = users.get(findU);
+        if (findU != null) {
+            for (Account a : listReq) {
+                if (a.getRequisite().equals(requisite)) {
+                    find = a;
+                }
+            }
+        }
+        return find;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = true;
+        Account accountSend = findByRequisite(srcPassport, srcRequisite);
+        Account accountDest = findByRequisite(destPassport, destRequisite);
+        if (accountSend == null || accountDest == null || accountSend.getBalance() < amount) {
+            rsl = false;
+        } else {
+            accountSend.setBalance(accountSend.getBalance() - amount);
+            accountDest.setBalance(accountDest.getBalance() + amount);
+        }
+        return rsl;
+    }
+}
