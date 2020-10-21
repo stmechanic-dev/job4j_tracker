80e3d43a1f96a39102c1389b04ffe4a8fcb749c8
stmechanic-dev <kovalev.ilya@mail.ru>
2020-10-22 00:30:46 +0300

    1. Организовать сортировку User [#358380]

---
+++ src/main/java/ru/job4j/tracker/collection/sort/User.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker.collection.sort;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        if (name.equals(o.getName())) {
+            return Integer.compare(age, o.getAge());
+        }
+        return name.compareTo(o.getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
