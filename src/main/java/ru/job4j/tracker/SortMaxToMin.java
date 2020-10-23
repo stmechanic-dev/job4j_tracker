f5f5a0442e5bb8e994b65a40393047f7130f5071
stmechanic <kovalev.ilya@mail.ru>
2020-10-23 13:52:47 +0300

    0. Сортировка [#358382]

--- src/main/java/ru/job4j/tracker/SortMaxToMin.java
+++ src/main/java/ru/job4j/tracker/SortMaxToMin.java
@@ -5,9 +5,14 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class SortMaxToMin {
+public class SortMaxToMin implements Comparator<Item> {
 
     public static void sort(List<Item> list) {
         Collections.sort(list, Collections.reverseOrder());
+    }
+
+    @Override
+    public int compare(Item first, Item second) {
+        return first.getName().compareTo(second.getName());
     }
 }
--- src/main/java/ru/job4j/tracker/SortMinToMax.java
+++ src/main/java/ru/job4j/tracker/SortMinToMax.java
@@ -1,10 +1,16 @@
 package ru.job4j.tracker;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class SortMinToMax {
+public class SortMinToMax implements Comparator<Item> {
     public static void sort(List<Item> list) {
         Collections.sort(list);
+    }
+
+    @Override
+    public int compare(Item first, Item second) {
+        return first.getName().compareTo(second.getName());
     }
 }
