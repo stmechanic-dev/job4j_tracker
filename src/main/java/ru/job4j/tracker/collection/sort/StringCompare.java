5ad202b7807983260610067fd31f07e28b108176
stmechanic-dev <kovalev.ilya@mail.ru>
2020-10-25 14:50:15 +0300

    3. Компаратор для строк [#358378]

--- src/main/java/ru/job4j/tracker/collection/sort/StringCompare.java
+++ src/main/java/ru/job4j/tracker/collection/sort/StringCompare.java
@@ -6,26 +6,15 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = 0;
-        if (left.length() == right.length()) {
-            for (int i = 0; i < left.length(); i++) {
-                result = Character.compare(left.charAt(i), right.charAt(i));
-                if (result != 0) {
-                    break;
-                }
+        int size = Math.min(left.length(), right.length());
+        for (int i = 0; i < size; i++) {
+            result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
+                break;
             }
-        } else {
-            int size = left.length() < right.length() ? left.length() : right.length();
-            for (int i = 0; i < size; i++) {
-                result = Character.compare(left.charAt(i), right.charAt(i));
-                if (result != 0) {
-                    break;
-                }
-            }
-            if (result == 0 && left.length() > right.length()) {
-                result = 1;
-            } else if (result == 0 && left.length() < right.length()) {
-                result = -1;
-            }
+        }
+        if (result == 0) {
+            result = Integer.compare(left.length(), right.length());
         }
 
         return result;
