60f5ee755df2d6219fc271330c0740f1a009a8e1
stmechanic-dev <kovalev.ilya@mail.ru>
2020-10-31 00:04:50 +0300

    1. Анонимные классы [#358210]

---
+++ src/main/java/ru/job4j/tracker/Block4/AttachmentSort.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.Block4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 13)
+        );
+        Comparator comparator =  new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Attachment left = (Attachment) o1;
+                Attachment right = (Attachment) o2;
+                return left.getSize() - right.getSize();
+            }
+        };
+
+        Comparator<Attachment> attComp = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+
+        attachments.sort(comparator.thenComparing(attComp));
+        System.out.println(attachments);
+        // Здесь создайте компаратор на основании анонимного класса.
+    }
+}
