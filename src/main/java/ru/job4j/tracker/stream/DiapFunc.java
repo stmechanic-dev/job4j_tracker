ab9a043a28747f79a517fd39a01e5339237873ed
stmechanic-dev <kovalev.ilya@mail.ru>
2020-11-08 16:23:40 +0300

    5. Подсчет функции в диапазоне. [#358205]

---
+++ src/main/java/ru/job4j/tracker/stream/DiapFunc.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.stream;
+
+import ru.job4j.tracker.Block4.Function;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiapFunc {
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> list = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            list.add(func.apply((double) i));
+        }
+        return list;
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/stream/DiapFuncTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DiapFuncTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = DiapFunc.diapason(5, 8, x -> x);
+        List<Double> expected = Arrays.asList(5D, 6D, 7D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLinearFunctionThenSquareResults() {
+        List<Double> result = DiapFunc.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLinearFunctionThenShowResults() {
+        List<Double> result = DiapFunc.diapason(5, 8, x -> Math.pow(x, x));
+        List<Double> expected = Arrays.asList(3125D, 46656D, 823543D);
+        assertThat(result, is(expected));
+    }
+
+}
