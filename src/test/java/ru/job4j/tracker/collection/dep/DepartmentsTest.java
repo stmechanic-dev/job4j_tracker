bddbfe34c4d4e83487510756c12f1c33666711fc
stmechanic-dev <kovalev.ilya@mail.ru>
2020-11-18 00:42:19 +0300

    1. Метод .of [#358231]

--- src/test/java/ru/job4j/tracker/collection/dep/DepartmentsTest.java
+++ src/test/java/ru/job4j/tracker/collection/dep/DepartmentsTest.java
@@ -11,7 +11,7 @@ public class DepartmentsTest {
     @Test
     public void whenMissed() {
         List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -19,7 +19,7 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -28,7 +28,7 @@ public class DepartmentsTest {
     public void whenUpDepartmentGoBefore() {
         List<String> input = Arrays.asList("K1/SK1/SSK1", "K2/SK1/SSK2", "K1/SK1/SSK2", "K2/SK1/SSK1"
                                             , "K1/SK2","K2/SK2" );
-        List<String> expect = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2"
+        List<String> expect = List.of("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2"
                                             , "K2/SK2", "K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
